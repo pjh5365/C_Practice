64f61d18304dfacbc12ace3287301ac007a5a908
pjh5365 <pjh5365@naver.com>
2022-01-30 17:49:38 +0900

    second commit

--- Second To hour/Second_to_hour.c
+++ Second To hour/Second_to_hour.c
@@ -1,32 +1,31 @@
 #include <stdio.h>
-int hour = 0, minute = 0, second = 0;	//포인터를 쓰면되지만 아직 기억이 안난다. 
 
-int second_to_minute(int a)
+int second_to_minute(int input, int * second, int * minute)
 {
 	while(1)	//입력받은 초가 0초가 될때까지 반복  
 	{
-		second++;
-		a--;
+		(*second)++;
+		input--;
 		 
-		if(second == 60)	//60초가되면 1분으로 변환  
+		if((*second) == 60)	//60초가되면 1분으로 변환  
 		{
-			second -= 60;
-			minute++;
+			(*second) -= 60;
+			(*minute)++;
 		}
 		
-		if(a == 0)
+		if(input == 0)
 			break;
 	}
 }
 
-int minute_to_hour(void)
+int minute_to_hour(int * minute, int * hour)
 {
 	while(1)
 	{	 
-		if(minute >= 60)	//분이 60분을 넘어가면 1시간으로 변환 
+		if((*minute) >= 60)	//분이 60분을 넘어가면 1시간으로 변환 
 		{
-			minute -= 60;
-			hour++;
+			(*minute) -= 60;
+			(*hour)++;
 		}
 		else
 		break;
@@ -35,22 +34,23 @@ int minute_to_hour(void)
 
 int main(void)
 {
-	int a;
+	int input;
+	int hour = 0, minute = 0, second = 0;
 	printf("변환할 초를 입력해주세요 : ");
 	
-	scanf("%d", &a);
+	scanf("%d", &input);
 	
-	if(a >= 60)
+	if(input >= 60)
 	{
-		second_to_minute(a);	//입력받은 초를 분으로 변환 
+		second_to_minute(input, &second, &minute);	//입력받은 초를 분으로 변환 
 		if(minute >= 60)		//분이 60분을 넘어갈때 시간으로 변환   
 		{
-			minute_to_hour();
+			minute_to_hour(&minute, &hour);
 		}
 	}
 	else
 	{
-		second = a;
+		second = input;
 	}
 	
 	printf("%d시 %d분 %d초\n", hour, minute, second);
