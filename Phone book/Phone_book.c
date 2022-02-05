dcafebfc7872b47dec4c22ac860f4f60889b7f9b
pjh5365 <98308611+pjh5365@users.noreply.github.com>
2022-02-05 19:51:57 +0900

    완성
    검색기능/정렬기능 구현완료

--- Phone book/Phone_book.c
+++ Phone book/Phone_book.c
@@ -3,32 +3,55 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+#define MAX_PERSON 100	//100명까지의 정보만저장 
 
 typedef struct phonebook{
-	char name[10];
+	char name[20];
 	char num[20];
 }Phone;
 
-void insertdata(void)	//정보를입력하는 함수 
+void Road_data(Phone *ptr, int *p_num)	//저장된 정보를 불러오는 함수 
 {
-	Phone input;
-	char exit;
-	FILE *fp = fopen("전화번호부.txt", "at");
+	FILE *fp = fopen("전화번호부.dat", "rt");
+	if(fp == NULL)
+	{
+	//	printf("파일오픈에러 \n");
+		return -1;
+	}
+	while(feof(fp) == 0)
+	{
+		fscanf(fp, "%s %s ",ptr[*p_num].name, ptr[*p_num].num);
+		(*p_num)++;
+	}
+	fclose(fp);
+}
+
+void Save_data(Phone *ptr, int num)	//입력된 정보를 데이터에 저장하는 함수 
+{
+	int i;
+	FILE *fp = fopen("전화번호부.dat", "wt");
 	if(fp == NULL)
 	{
 		printf("파일오픈에러 \n");
 		return -1;
 	}
+	for(i = 0; i < num; i++)
+		fprintf(fp, "%s %s ", ptr[i].name, ptr[i].num);
+		
+	fclose(fp);
+} 
+
+void insert_data(Phone *ptr, int *p_num)	//정보를입력하는 함수
+{
+	char exit;
 	while(1)
 	{
 		fputs("이름과 전화번호를 입력해주세요 (EX)홍길동 010-1234-1234) \n", stdout);
 		fputs("이름과 전화번호 : ", stdout);
-		scanf("%s %s",input.name, input.num);
-		fputs(input.name, fp);
-		fputc(' ', fp);
-		fputs(input.num, fp);
-		fputc('\n', fp);
+		scanf("%s %s",ptr[*p_num].name, ptr[*p_num].num);
+		(*p_num)++;
 		getchar();
 		fputs("돌아가려면 n을 입력하세요 : ", stdout);
 		scanf("%c", &exit);
@@ -38,117 +61,148 @@ void insertdata(void)	//정보를입력하는 함수
 		if(exit == 'n' || exit == 'N')
 			break; 
 	}
-	
-	fclose(fp);
-	return;
-}
-/*
-void deletedata()	//정보를 삭제하는 함수  
-{
-	Phone buf[100];
-	FILE *fp1 = fopen("전화번호부.txt", "rt");
-	buf = fp1; 
-	FILE *fp2 = fopen("전화번호부.txt", "wt");
-	if(fp1 == NULL || fp2 == NULL)
-	{
-		printf("파일오픈에러 \n");
-		return -1;
-	}
-}
-*/
-void searchdata(void)	//이름으로 검색하는 함수 
-{
-	char search[10];
-	char buf[10];
-	
-	FILE *fp = fopen("전화번호부.txt", "rt");
-	if(fp == NULL)
-	{
-		printf("파일오픈에러 \n");
-		return -1;
-	}
-	printf("검색할 이름을 입력하세요 : ");
-	scanf("%s", search);
-	getchar();
-	while(feof(fp) == 0)	//파일끝까지도달하지 않았을때 실행 
-	{
-		fscanf(fp, "%s", buf);
-		if(strcmp(buf, search) == 0)
-		{
-			printf("%s ",buf);	//이름출력 
-			fscanf(fp, "%s", buf);
-			printf("%s \n\n", buf);	//전화번호출력 
-			break;
-		}
-	}
-	fclose(fp);
-	system("PAUSE");
-	system("cls");
 	return;
 }
 
-void print_all(void)	//전화번호부를 출력시키는 함수 
+void delete_data(Phone *ptr, int *p_num)	//정보를 삭제하는 함수  
 {
-	char ch;
-	FILE *fp = fopen("전화번호부.txt", "rt");
-	if(fp == NULL)
+	int i, j;
+	char name[20];
+	fputs("삭제할 이름을 입력해주세요 : ", stdout);
+	scanf("%s", name);
+	getchar();
+	for(i = 0; i < (*p_num); i++)
 	{
-		printf("파일오픈에러 \n");
-		return -1;
+		if(strcmp(name, ptr[i].name) == 0)
+		{
+			for(j=i+1; j < *p_num; j++)
+			{
+				strcpy(ptr[j-1].name, ptr[j].name);
+				strcpy(ptr[j-1].num, ptr[j].num);
+			}
+			(*p_num)--;
+			system("cls");
+			return;
+		}
 	}
-	while(feof(fp) == 0)	//파일끝까지도달하지 않았을때 실행 
+	fputs("해당정보를 찾을 수 없습니다. \n", stdout);
+	system("PAUSE");
+	system("cls");
+}
+
+void search_data(Phone *ptr, int p_num)	//이름으로 검색하는 함수
+{
+	char search[10];
+	int i;
+	
+	printf("검색할 이름을 입력하세요 : ");
+	scanf("%s", search);
+	getchar();
+	
+	for(i = 0; i < p_num; i++)
 	{
-		ch = fgetc(fp);
-		printf("%c", ch);
+		if(strcmp(ptr[i].name, search) == 0)
+		{
+			printf("\n%s %s \n\n",ptr[i].name, ptr[i].num);
+			system("PAUSE");
+			system("cls");
+			return;
+		}
 	}
-	fclose(fp);
+	printf("입력한 정보를 찾지못했습니다. \n\n");
+	system("PAUSE");
+	system("cls");
+}
+
+void print_all(Phone *ptr, int p_num)	//전화번호부를 출력시키는 함수 
+{
+	int i;
+	for(i = 0; i < p_num; i++)
+		printf("%s %s \n",ptr[i].name, ptr[i].num);
+		
 	printf("\n");
 	system("PAUSE");
 	system("cls");
-	return;
+}
+
+void sort_data(Phone *ptr, int p_num)
+{
+	int i, j;
+	Phone tmp;
+	for(i = p_num -1; i > 0; i--)
+	{
+		for(j = 0; j <i; j++)
+		{
+			if(strcmp(ptr[j].name, ptr[j+1].name) > 0)	//앞의 이름이 더크면 0보다 큰 값을 반환 
+			{
+				strcpy(tmp.name, ptr[j].name);
+				strcpy(tmp.num, ptr[j].num);
+				strcpy(ptr[j].name, ptr[j+1].name);
+				strcpy(ptr[j].num, ptr[j+1].num);
+				strcpy(ptr[j+1].name, tmp.name);
+				strcpy(ptr[j+1].num, tmp.num );
+			}
+		}
+	}
+	fputs("이름순으로 정렬이 완료되었습니다 \n\n", stdout);
+	system("PAUSE");
+	system("cls");
 }
 
 int main(void)
 {
-	int inputnum;
+	Phone ptr[MAX_PERSON];	//데이터저장을 위한 배열
+	int person_num = 0;
+	int menu;
+	
+	Road_data(ptr, &person_num);
+	 
 	while(1)
 	{
 		printf("--------전화번호부-------- \n");
 		printf("1.입력 \n2.삭제 \n3.검색 \n4.전체출력 \n5.정렬 \n6.종료\n");
 		printf("\n\n번호를 선택해주세요 : ");
-		scanf("%d", &inputnum);
+		scanf("%d", &menu);
 		getchar();
 		system("cls");
 		
-		switch(inputnum){
+		switch(menu){
 			
 			case 1:
 			{
-				insertdata();
+				insert_data(ptr, &person_num);
 				break;
 			}
 			
 			case 2:
 			{
-			//	deletedata();
+				delete_data(ptr, &person_num);
 				break;
 			}
 				
 			case 3:
 			{
-				searchdata();
+				search_data(ptr, person_num);
 				break;
 			}
 				
 			case 4:
 			{
-				print_all();
+				print_all(ptr, person_num);
 				break;		
+			}
+			
+			case 5:
+			{
+				sort_data(ptr, person_num);
+				break;
 			}
 				
 			case 6:
+			{
+				Save_data(ptr, person_num);
 				return 0; 
-				
+			}
 		}
 	}
 	return 0;
